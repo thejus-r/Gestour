0af8816e998674008aab2f39f63e6dd832858613
Thejus Rajendran <thejusr1999@gmail.com>
2022-06-10 10:22:52 +0530

    - fixed the async issue faced by the child process using Promises

--- middleware/thread.cjs
+++ middleware/thread.cjs
@@ -1,17 +1,33 @@
+const { PythonShell } = require("python-shell");
+
 module.exports = async (text) => {
-  const spawn = require("child_process").spawn;
-
-  const ls = spawn("python", ["emotionAnalyser.py", text]);
-
-  var showData;
-
-  ls.stdout.on("data", (data) => {
-    showData = `${data}`;
-
-    showData = JSON.parse(showData);
-    console.log("from thread.js ", showData);
-
-    return showData;
-  });
-  return Promise.resolve(showData);
+  const status = ({
+    success,
+    err = "",
+    result,
+  } = await new Promise((resolve, reject) => {
+    PythonShell.run(
+      "emotionAnalyser.py",
+      { args: [text] },
+      function (err, result) {
+        if (err) {
+          reject({ success: false, err });
+        }
+        resolve({ success: true, result });
+      }
+    );
+  }));
+  return status;
 };
+
+// module.exports = async (text) => {
+//   const spawn = require("child_process").spawn;
+
+//   const ls = spawn("python", ["emotionAnalyser.py", text]);
+
+//   ls.stdout.on("data", async (data) => {
+//     var showData = `${data}`;
+//     //console.log(showData);
+//     return await showData;
+//   });
+// };
